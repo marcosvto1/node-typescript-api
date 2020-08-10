bb3d0829504ec41af5f226987269e7bcaff58ba7
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-10 14:22:18 -0300

    add global type

---
+++ test/jest-setup.ts
@@ -0,0 +1,8 @@
+import { SetupServe } from '@src/server';
+import  supertest  from 'supertest';
+
+beforeAll(() => {
+  const server = new SetupServe();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
