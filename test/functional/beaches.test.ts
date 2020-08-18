779e8d4bba67c431c2b9746d8284a6aa9ad41f97
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-18 14:11:18 -0300

    to integrate controller with service forecast

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -20,5 +20,52 @@ describe('Beaches functional tests', () => {
       // verifica se neste objecto contém o newBeach, pois ainda vai ter id dinamico gerado pelo banco
       expect(response.body).toEqual(expect.objectContaining(newBeach));
     });
+
+    it('should return 422 when there is a validation error', async() => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E'
+      };
+
+      const response = await global.testRequest.post(
+        '/beaches'
+      ).send(newBeach);
+
+      expect(response.status).toBe(422);
+      // verifica se neste objecto contém o newBeach, pois ainda vai ter id dinamico gerado pelo banco
+      expect(response.body).toEqual({
+        error: 
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" at path "lat"',
+      });
+    });
+
+    it('should return 500 when there is any error other then validation error', async () => {
+
+      jest
+        .spyOn(Beach.prototype, 'save')
+        .mockImplementationOnce(() => Promise.reject('fail to create beach'))
+
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E'
+      };
+  
+      const response = await global.testRequest.post(
+        '/beaches'
+      ).send(newBeach);
+
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({
+        error: 'Internal server Error'
+      });
+
+    });
+
   });
+
+  
 }); 
