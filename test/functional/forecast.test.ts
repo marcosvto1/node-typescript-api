e057681c62cbf9e4e3f2558becbd5d6458c84a07
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-18 14:22:43 -0300

    should return 500 if something goes wrong during the processing

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,4 +1,5 @@
 import { Beach, BeachPosition } from '@src/models/beach';
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import nock from 'nock';
 
@@ -40,68 +41,31 @@ describe('Beach forecast functional tests', () => {
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
 
-    expect(body).toEqual([
-      {
-        time: '2020-04-26T00:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 64.26,
-            swellHeight: 0.15,
-            swellPeriod: 3.89,
-            time: '2020-04-26T00:00:00+00:00',
-            waveDirection: 231.38,
-            waveHeight: 0.47,
-            windDirection: 299.45,
-            windSpeed: 100
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T01:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: 'E',
-            rating: 1,
-            swellDirection: 123.41,
-            swellHeight: 0.21,
-            swellPeriod: 3.67,
-            time: '2020-04-26T01:00:00+00:00',
-            waveDirection: 232.12,
-            waveHeight: 0.46,
-            windDirection: 310.48,
-            windSpeed: 100
-          },
-        ],
-      },
-      {
-        time: '2020-04-26T02:00:00+00:00',
-        forecast: [
-          {
-            lat: -33.792726,
-            lng: 151.289824,
-            name: 'Manly',
-            position: BeachPosition.E,
-            rating: 1,
-            swellDirection: 182.56,
-            swellHeight: 0.28,
-            swellPeriod: 3.44,
-            time: '2020-04-26T02:00:00+00:00',
-            waveDirection: 232.86,
-            waveHeight: 0.46,
-            windDirection: 321.5,
-            windSpeed: 100,
-          },
-        ],
-      },
-    ]);
+    expect(body).toEqual(apiForecastResponse1BeachFixture);
 
   });
+
+  it('should return 500 if something goes wrong during the processing', async () => {
+    nock('https://api.stormglass.io:443',
+    {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      }
+    })
+    .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+    .get('/v1/weather/point')
+    .query({
+      lat:'-33.792726',
+      lng:'151.289824',
+
+    })
+    .replyWithError('Something went wrong')
+
+    const { status } = await global.testRequest.get('/forecast');
+
+    expect(status).toBe(500);
+
+  })
+
 });
