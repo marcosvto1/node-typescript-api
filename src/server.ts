4168f5df685a4c456ad1ba66f20280c0d99214b3
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-18 17:13:57 -0300

    refactor imports

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,42 @@
-import { Server } from '@overnightjs/core';
-import { BeachesController } from '@src/controllers/beaches';
-import { ForecastController } from '@src/controllers/forecast';
-import * as database from '@src/database';
-import bodyParser from 'body-parser';
-import { Application } from 'express';
 import './util/module-alias';
 
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { Application } from 'express';
+import { BeachesController } from '@src/controllers/beaches';
+import { ForecastController } from '@src/controllers/forecast';
+import { UsersController } from '@src/controllers/users';
 
-export class SetupServe extends Server {
+import * as database from './database';
+
+
+
+export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
   public async init(): Promise<void> {
     this.setupExpress();
-    this.setupController();
+    this.setupControllers();
     await this.databaseSetup();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.setupControllers();
   }
 
-  private setupController(): void {
+  private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
+    const usersController = new UsersController();
+    
+    this.addControllers([
+      forecastController, 
+      beachesController,
+      usersController
+    ]);
   }
 
   private async databaseSetup(): Promise<void> {
@@ -37,6 +48,12 @@ export class SetupServe extends Server {
     // TODO - vai desligar toda aplicacao
   }
 
+  public start(): void {
+    this.app.listen( this.port, () => {
+      console.info('Server listening of port:', this.port);
+    });
+  }
+ 
   public getApp(): Application {
     return this.app;
   }
