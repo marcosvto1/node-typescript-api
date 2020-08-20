4e5f6261f600c41aa330d34b6ab71c49cdd62b08
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-20 12:52:40 -0300

    use auth configs in method generateToken

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import config from 'config';
 
 export default class AuthService {
 
@@ -16,8 +17,8 @@ export default class AuthService {
   }
 
   public static generateToken(payload: object): string {
-    return jwt.sign(payload, 'teste', {
-      expiresIn: 10000
+    return jwt.sign(payload, config.get('App.auth.key'), {
+      expiresIn: config.get('App.auth.tokenExpiresIn')
     });
   }
 }
