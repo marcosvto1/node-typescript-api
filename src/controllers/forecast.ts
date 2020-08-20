e7e7cdcb42e3b0907d844c67f12056fb44db9b95
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-20 15:27:33 -0300

    insert middleware in ForecastController

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,16 +1,20 @@
-import { Controller, Get } from '@overnightjs/core';
+import { Controller, Get, ClassMiddleware } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '../models/beach';
 import { Forecast } from './../services/forecast';
+import { authMiddleware } from '@src/middlewares/auth';
 
 const forecast = new Forecast(); 
 
 @Controller('forecast')
+@ClassMiddleware(authMiddleware)
 export class ForecastController {
   @Get('')
-  public async getForecastForLoggedUser(_: Request, res: Response): Promise<void> {
+  public async getForecastForLoggedUser(req: Request, res: Response): Promise<void> {
     try{
-      const beaches = await Beach.find({});
+      const beaches = await Beach.find({
+        user: req.decoded?.id
+      });
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     }catch(error) {
