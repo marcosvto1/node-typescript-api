138249d90ceee3dd09f24f3f2e39a52c23391bf9
Marcos Tomaz <marcosvto1@gmail.co>
2020-08-20 11:25:36 -0300

    add function handle error response client mongoose

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,17 +1,32 @@
+import { CUSTOM_VALIDATION } from './../models/user';
 import mongoose from 'mongoose';
 import { Response} from 'express';
 
 export abstract class BaseController {
   protected sendCreatedUpdatedErrorResponse(
     res: Response, 
-    error: mongoose.Error.ValidationError | Error): Response {
-
+    error: mongoose.Error.ValidationError | Error): void {
     if (error instanceof mongoose.Error.ValidationError) {
-      res.status(422).send({code: 422, error: error.message});
+      const clientErrors = this.handleClientErrors(error);
+      res
+        .status(clientErrors.code)
+        .send(clientErrors);
     } else {
       res.status(500).send({code: 500, error: 'Something went wrong'});
     }
-
-    return res;
   } 
+
+  private handleClientErrors(
+    error: mongoose.Error.ValidationError): 
+    {code: number, error: string} {
+      const duplicateKindErrors = Object.values(error.errors).filter((error) => error.kind === CUSTOM_VALIDATION.DUPLICATED)
+      if (duplicateKindErrors.length) {
+        return {
+          code: 409,
+          error: error.message
+        };
+      }
+      
+      return {code: 422, error: error.message};
+  }
 }
